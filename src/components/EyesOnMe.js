a8af1c773e78b68919ddfd8658596414d0446195
Stella Mazzillo <stellagress5@gmail.com>
2023-05-22 19:12:52 -0400

    plays around

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -12,7 +12,6 @@ export default EyesOnMe
 
 
 
-
 // On that button, add event handlers that listen for the focus and blur events.
 // When the focus event fires, use console.log to print out the text 'Good!'.
 // When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
